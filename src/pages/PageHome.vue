9b405b8ad44f35707d38a56e8f61bddf1263f0dc
scripturesdev <scripturesdev@gmail.com>
2023-08-09 20:52:57 +0200

    new updates

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,26 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>home page</h5>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="/src/statics/Avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Scripturesdev</q-item-label>
+          <q-item-label caption> Gothenburg, Sweden </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+      <q-img class="col" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-card-section>
+        <div>Golden Gate Bridge</div>
+        <div class="text-caption text-grey">Jun 10 9:33AM</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -11,3 +31,11 @@ export default defineComponent({
   name: "PageHome",
 });
 </script>
+
+<style lang="scss">
+.card-post {
+  .q-img {
+    min-height: 200px;
+  }
+}
+</style>
