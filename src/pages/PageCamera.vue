cd92a6225eaf14c5e397873aea36d76882cb2aba
scripturesdev <scripturesdev@gmail.com>
2023-08-23 21:31:03 +0200

    camera stream added

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,38 +1,94 @@
 <template>
-  <q-page class="constrain q-pa-md">
-    <q-card class="my-card" flat bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-        </q-item-section>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
+      <canvas
+        v-show="imageCaptured"
+        ref="canvas"
+        class="full-width"
+        height="240"
+      >
+      </canvas>
+    </div>
 
-        <q-item-section>
-          <q-item-label>Title</q-item-label>
-          <q-item-label caption> Subhead </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator />
-
-      <q-card-section horizontal>
-        <q-card-section>
-          {{ lorem }}
-        </q-card-section>
-
-        <q-separator vertical />
-
-        <q-card-section class="col-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </q-card-section>
-      </q-card-section>
-    </q-card>
+    <div class="text-center q-pa-md">
+      <q-btn
+        @click="captureImage"
+        round
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+      />
+    </div>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        class="col col-sm-6"
+        v-model="post.caption"
+        label="Caption"
+        dense
+      />
+    </div>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        class="col col-sm-6"
+        v-model="post.location"
+        label="location"
+        dense
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="eva-navigation-2-outline" />
+        </template>
+      </q-input>
+    </div>
+    <div class="row justify-center q-mt-lg">
+      <q-btn unelevated rounded color="grey-10" label="Submit" />
+    </div>
   </q-page>
 </template>
 
-<script>
-export default {
-  name: "PageCamera",
-};
+<script setup>
+import { uid } from "quasar";
+import { ref, computed, onMounted } from "vue";
+//require("md-gum-polyfill");
+
+const post = ref({
+  id: uid(),
+  caption: "",
+  location: "",
+  photo: null,
+  date: Date.now(),
+});
+
+const video = ref(null);
+const canvas = ref(null);
+const imageCaptured = ref(false);
+
+function initCamera() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+    })
+    .then((stream) => {
+      video.value.srcObject = stream;
+    });
+}
+onMounted(() => {
+  initCamera();
+  imageCaptured.value = false;
+});
+
+function captureImage() {
+  canvas.value.width = video.value.getBoundingClientRect().width;
+  canvas.value.height = video.value.getBoundingClientRect().height;
+  let context = canvas.value.getContext("2d");
+  context.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height);
+  imageCaptured.value = true;
+  canvas.value.post.photo = canvas.value.toDataURL();
+}
 </script>
+
+<style lang="sass">
+.camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,38 +1,57 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="/src/statics/Avatar.png" />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="/src/statics/Avatar.png" />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">Scripturesdev</q-item-label>
-          <q-item-label caption> {{ post.location }} </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">Scripturesdev</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
-      <q-img :src="post.imageUrl" />
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption">{{ niceDate(post.date) }}</div>
-      </q-card-section>
-    </q-card>
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-4">
+        <q-item class="fixed large-screen-only">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="/src/statics/Avatar.png" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Scripturesdev</q-item-label>
+            <q-item-label caption> Scripturesdev </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import { date } from "quasar";
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 
 const posts = ref([
   {
@@ -63,12 +82,13 @@ const posts = ref([
     location: "San Fransisco, US",
     imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
   },
-])
+]);
 
 // cant used with computed with arguments. this is the same as the function below
 //function niceDate (value) {return date.formatDate(value, "MMMM D h:mmA")}
-const niceDate = (value) => { return date.formatDate(value, "MMMM D h:mmA") }
-
+const niceDate = (value) => {
+  return date.formatDate(value, "MMMM D h:mmA");
+};
 </script>
 
 <style lang="scss">
