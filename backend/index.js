60be021f7617f200bc9613b40ea52ccab9467eb5
scripturesdev <scripturesdev@gmail.com>
2023-08-31 17:10:09 +0200

    added getPost and error handling

--- backend/index.js
+++ backend/index.js
@@ -3,35 +3,62 @@
 */
 
 const express = require("express");
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
 
 /*
   config - express
 */
 
+const app = express();
+const port = 3000;
+
+/*
+  config -firebase
+*/
+
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require("firebase-admin/firestore");
+const serviceAccount = require("./keys/quasagram-SAkey.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
 /*
   endpoint - posts
 */
+
+app.get("/posts", (request, response) => {
+  //fix CORS error
+  response.set("Access-Control-Allow-Origin", "*");
+
+  let posts = [];
+  db.collection("posts")
+    .orderBy("date", "desc")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        posts.push(doc.data());
+      });
+      response.send(posts);
+    });
+});
 
 /*
   listen
 */
 
-const app = express();
-const port = 3000;
-
-app.get("/posts", (request, response) => {
-  let posts = [
-    {
-      caption: "Golden Gate Bridge",
-      location: "San Fransisco",
-    },
-    {
-      caption: "Golden Gate Bridge",
-      location: "San Fransisco",
-    },
-  ];
-  response.send(posts);
-});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   console.log("our endpoint is working");
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,35 +2,69 @@
   <q-page class="constrain q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-8">
-        <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="card-post q-mb-md"
-          flat
-          bordered
+        <template v-if="!loadingPosts && posts.length">
+          <q-card
+            v-for="post in posts"
+            :key="post.id"
+            class="card-post q-mb-md"
+            flat
+            bordered
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="/src/statics/Avatar.png" />
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-bold">Scripturesdev</q-item-label>
+                <q-item-label caption> {{ post.location }} </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+            <q-img :src="post.imageUrl" />
+            <q-card-section>
+              <div>{{ post.caption }}</div>
+              <div class="text-caption text-grey">
+                {{ date.formatDate(post.date, "MMMM D h:mmA") }}
+              </div>
+            </q-card-section>
+          </q-card>
+        </template>
+        <template v-else-if="!posts.length && !loadingPosts"
+          >No posts yet...</template
         >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="/src/statics/Avatar.png" />
-              </q-avatar>
-            </q-item-section>
+        <template v-else>
+          <q-card flat bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton size="40px" type="QAvatar" animation="fade" />
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label class="text-bold">Scripturesdev</q-item-label>
-              <q-item-label caption> {{ post.location }} </q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" animation="fade" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
-          <q-img :src="post.imageUrl" />
-          <q-card-section>
-            <div>{{ post.caption }}</div>
-            <div class="text-caption text-grey">
-              {{ date.formatDate(post.date, "MMMM D h:mmA") }}
-            </div>
-          </q-card-section>
-        </q-card>
+            <q-skeleton height="200px" square animation="fade" />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" animation="fade" />
+              <q-skeleton
+                type="text"
+                width="50%"
+                class="text-subtitle2"
+                animation="fade"
+              />
+            </q-card-section> </q-card
+        ></template>
       </div>
 
       <div class="col-4">
@@ -53,38 +87,31 @@
 
 <script setup>
 import { date } from "quasar";
-import { ref, computed } from "vue";
+import { computed, ref } from "vue";
+import axios from "axios";
+import { useQuasar } from "quasar";
 
-const posts = ref([
-  {
-    id: 1,
-    caption: "Golden gate Bridge",
-    date: 1691607808765,
-    location: "San Fransisco, US",
-    imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-  },
-  {
-    id: 2,
-    caption: "Golden gate Bridge",
-    date: 1691607808765,
-    location: "San Fransisco, US",
-    imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-  },
-  {
-    id: 3,
-    caption: "Golden gate Bridge",
-    date: 1691607808765,
-    location: "San Fransisco, US",
-    imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-  },
-  {
-    id: 4,
-    caption: "Golden gate Bridge",
-    date: 1691607808765,
-    location: "San Fransisco, US",
-    imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-  },
-]);
+const $q = useQuasar();
+const posts = ref([]);
+const loadingPosts = ref(false);
+
+function getPosts() {
+  loadingPosts.value = true;
+  axios
+    .get("http://localhost:3000/posts")
+    .then((response) => {
+      posts.value = response.data;
+      loadingPosts.value = false;
+    })
+    .catch((err) => {
+      $q.dialog({
+        title: "Error",
+        message: "Could not download posts",
+      });
+    });
+}
+
+getPosts();
 
 // cant used with computed with arguments. this is the same as the function below
 //function niceDate (value) {return date.formatDate(value, "MMMM D h:mmA")}
